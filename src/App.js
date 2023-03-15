8c16e3c82fe821b413c1aa2fabf60d9bd5170a69
BenHbrt <benherbert1983@googlemail.com>
2023-03-15 17:51:39 +0100

    Click -first iteration

--- src/App.js
+++ src/App.js
@@ -2,113 +2,51 @@ import './App.scss';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { useState } from 'react'
 
-const itemsFromBackend = [
-  {id: "item1", content: "First Task"},
-  {id: "item2", content: "Second Task"}
-]
-
-const columnsFromBackend = {
-  col1: {
-    name: "Todo",
-    items: itemsFromBackend
-  },
-  col2: {
-    name: "In Progress",
-    items: []
-  }
-}
-
-const onDragEnd = (result, columns, setColumns) => {
-  if(!result.destination) return;
-  const { source, destination } = result
-  if(source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId]
-    const destColumn = columns[destination.droppableId]
-    const sourceItems = [...sourceColumn.items]
-    const destItems = [...destColumn.items]
-    const [removed] = sourceItems.splice(source.index, 1)
-    destItems.splice(destination.index, 0, removed)
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    })
-  } else {
-    const column = columns[source.droppableId]
-    const copiedItems = [...column.items]
-    const [removed] = copiedItems.splice(source.index, 1)
-    copiedItems.splice(destination.index, 0, removed)
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    })
-  } 
-}
-
 function App() {
 
-  const [columns, setColumns] = useState(columnsFromBackend)
+  const [answer, setAnswer] = useState([])
+  const [question, setQuestion] = useState(["This", "is", "a", "sentence"])
+
+  const move = (word, index) => {
+    const newAnswer = [...answer]
+    newAnswer.push(word)
+    setAnswer(newAnswer)
+    const newQuestion = [...question]
+    newQuestion.splice(index, 1)
+    setQuestion(newQuestion)
+  }
+
+  const moveBack = (word, index) => {
+    const newQuestion = [...question]
+    newQuestion.push(word)
+    setQuestion(newQuestion)
+    const newAnswer = [...answer]
+    newAnswer.splice(index, 1)
+    setAnswer(newAnswer)
+  }
 
   return (
     <div className="app">
-      <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
-        {Object.entries(columns).map(([id, column]) => {
+      <div className="container">
+        {answer.map((word, i) => {
           return (
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-              <h2>{column.name}</h2>
-              <div style={{margin: "8px"}}>
-            <Droppable droppableId={id} key={id}>
-              {(provided, snapshot) => {
-                return (
-                  <div {...provided.droppableProps} ref={provided.innerRef} 
-                  style={{
-                    background: snapshot.isDraggingOver ? 'lightblue': 'lightgrey',
-                    padding: 4,
-                    width: 250,
-                    minHeight: 500
-                  }}>
-                    {column.items.map((item, index) => {
-                      return (
-                        <Draggable key={item.id} draggableId={item.id} index={index}>
-                          {(provided, snapshot) => {
-                            return (
-                              <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}
-                              style={{
-                                userSelect: 'none',
-                                padding: 16,
-                                margin: '0 0 8px 0',
-                                minHeight: '50px',
-                                backgroundColor: snapshot.isDragging ? 'lightgreen' : 'lightblue',
-                                ...provided.draggableProps.style
-                              }}>
-                                {item.content}
-                              </div>
-                            )
-                          }}
-                        </Draggable>
-                      )
-                    })}
-                    {provided.placeholder}
-                  </div>
-                )
-              }}
-            </Droppable>
-            </div>
+            <div className="word" key={i} onClick={() => moveBack(word, i)}>
+              {word}
             </div>
           )
         })}
-      </DragDropContext>
+      </div>
+      <div className="container">
+        {question.map((word, i) => {
+            return (
+              <div className="word" key={i} onClick={() => move(word, i)}>
+                {word}
+              </div>
+            )
+        })} 
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
